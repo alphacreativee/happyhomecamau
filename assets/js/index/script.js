0220c89fd9ac3d02fe9086d733560753c92f7c9a
HuuBang11 <huubang0312@gmail.com>
2025-12-27 16:09:32 +0700

    update landing page

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -11,9 +11,88 @@ gsap.ticker.add((time) => {
 
 gsap.ticker.lagSmoothing(0);
 
+function hero() {
+  if ($("section.hero").length < 1) return;
+
+  $(".hero-slider").each(function () {
+    let $slider = $(this);
+
+    let $dataSpeed;
+    let $dataLoop = $slider.attr("data-loop");
+    let $dataAutoplay = $slider.data("autoplay")
+      ? { delay: $slider.data("autoplay") }
+      : $slider.data("autoplay");
+    if ($slider.is("[data-speed]")) {
+      $dataSpeed = $slider.data("speed");
+    } else {
+      $dataSpeed = 900; // by default
+    }
+
+    new Swiper($slider[0], {
+      direction: "vertical",
+      speed: $dataSpeed,
+      loop: $dataLoop,
+      autoplay: $dataAutoplay,
+      preloadImages: true,
+      parallax: true,
+      lazy: {
+        loadPrevNext: true
+      },
+      allowTouchMove: false,
+      simulateTouch: false,
+      mousewheel: false,
+      pagination: {
+        el: ".hero .swiper-pagination",
+        clickable: false,
+        renderBullet: function (i, className) {
+          return `
+            <button class="${className}">
+            <svg class="progress" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle class="circle-origin" cx="14" cy="14" r="13" stroke="white"/>
+            </svg>
+            </button>`;
+        }
+      },
+      navigation: {
+        nextEl: ".hero .swiper-button-next",
+        prevEl: ".hero .swiper-button-prev"
+      },
+      on: {
+        init: function () {
+          let $this = this;
+          $($this.slides[$this.activeIndex]);
+        }
+      }
+    });
+  });
+
+  gsap.utils.toArray("[effect-title]").forEach((description) => {
+    const delay = parseFloat(description.getAttribute("data-delay")) || 0;
+
+    const split = new SplitText(description, {
+      type: "lines",
+      linesClass: "line",
+      mask: "lines"
+    });
+
+    gsap.fromTo(
+      split.lines,
+      { yPercent: 100, willChange: "transform" },
+      {
+        yPercent: 0,
+        duration: 1.5,
+        ease: "power3.out",
+        stagger: 0.05,
+        delay: delay
+      }
+    );
+  });
+}
+
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
   customDropdown();
+  hero();
 };
 preloadImages("img").then(() => {
   init();
