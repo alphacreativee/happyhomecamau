9c98a252a484ba67a88b3b8d852aab0ae961ae62
Taoo <hai.alphacreative@gmail.com>
2025-12-27 14:07:43 +0700

    add new src

---
+++ assets/js/index/script.js
@@ -0,0 +1,34 @@
+import { preloadImages } from "../../libs/utils.js";
+import { customDropdown } from "../../js/index/global.js";
+("use strict");
+$ = jQuery;
+
+const lenis = new Lenis();
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+
+const init = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  customDropdown();
+};
+preloadImages("img").then(() => {
+  init();
+});
+
+let isLinkClicked = false;
+$("a").on("click", function (e) {
+  if (this.href && !this.href.match(/^#/) && !this.href.match(/^javascript:/)) {
+    isLinkClicked = true;
+  }
+});
+
+$(window).on("beforeunload", function () {
+  if (!isLinkClicked) {
+    $(window).scrollTop(0);
+  }
+  isLinkClicked = false;
+});
